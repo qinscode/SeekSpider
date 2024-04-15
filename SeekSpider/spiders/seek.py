fda3206da27f17cb5340c78a6fb09a41736e6220
realfudong <fudong.qin@outlook.com>
2024-04-15 09:22:27 +0800

    modified

--- SeekSpider/spiders/seek.py
+++ SeekSpider/spiders/seek.py
@@ -13,7 +13,7 @@ class SeekSpider(scrapy.Spider):
     'siteKey': 'AU-Main',
     'sourcesystem': 'houston',
     'where': 'All Sydney NSW',
-    'page': 2,
+    'page': 1,
     'seekSelectAllPages': 'true',
     'classification': '6281',
     'subclassification': '',
