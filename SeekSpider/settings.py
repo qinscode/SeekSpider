8e53675abdb53c9e9f9322148264fb633a1ee246
realfudong <fudong.qin@outlook.com>
2024-04-15 08:29:12 +0800

    exceed 26 pages limit

--- SeekSpider/settings.py
+++ SeekSpider/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = "SeekSpider.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
